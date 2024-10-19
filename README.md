9136a6505572dc4c1efdb1ec42d0cbfdb6a6499a
semihgecten <semihgencten@gmail.com>
2024-10-19 23:00:49 +0300

    repo and db is set up

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Dataset:
 https://www.kaggle.com/datasets/irkaal/foodcom-recipes-and-reviews/data
+
+intellijide ultimate versiyonu okul emaili ile lisanslayıp kurmanız önerilir
+
+to run database:
+in root folder, run:
+docker-compose up
---
+++ schema.sql
@@ -0,0 +1,7 @@
+-- User table for user-management-api
+CREATE TABLE user (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       username VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL,
+                       password VARCHAR(255) NOT NULL
+);
