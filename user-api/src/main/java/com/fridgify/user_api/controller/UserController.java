1b212ba2768d7f17d5cd12c21f3ed74458ef3158
remauxl <101212641+remauxl@users.noreply.github.com>
2024-10-30 19:30:26 +0300

    login and register endpoints

---
+++ user-api/src/main/java/com/fridgify/user_api/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.fridgify.user_api.controller;
+import com.fridgify.user_api.dto.UserDTO;
+import com.fridgify.user_api.model.User;
+import com.fridgify.user_api.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/user-api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserDTO userDTO) {
+        String response = userService.registerUser(userDTO);
+        if ("User registered successfully".equals(response)) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody UserDTO userDTO) {
+        String response = userService.loginUser(userDTO);
+        if ("Login successful".equals(response)) {
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+    }
+
+    @GetMapping("/find/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        User user = userService.findByUsername(username);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("User API is working");
+    }
+
+
+
+
+
+}
