a00c49d90632551b2d0b765624137068a036e78b
Enceladus-py <dalsunaberat@gmail.com>
2024-11-06 00:13:36 +0300

    new: return types and err codes, refactor

--- auth-api/src/main/java/com/fridgify/auth_api/controller/AuthController.java
+++ auth-api/src/main/java/com/fridgify/auth_api/controller/AuthController.java
@@ -1,48 +1,68 @@
 package com.fridgify.auth_api.controller;
 
 import com.fridgify.auth_api.client.UserServiceClient;
-import com.fridgify.auth_api.common.UserDTO;
-import com.fridgify.auth_api.dto.LoginRequest;
+import com.fridgify.auth_api.dto.LoginUserDTO;
+import com.fridgify.auth_api.dto.RegisterUserDTO;
+import com.fridgify.auth_api.dto.ResponseUserDTO;
+import com.fridgify.auth_api.dto.TokenDTO;
 import com.fridgify.shared.jwt.util.JwtUtil;
+import feign.FeignException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth-api")
 public class AuthController {
 
     private final JwtUtil jwtUtil;
-    private final BCryptPasswordEncoder passwordEncoder;
     private final UserServiceClient userServiceClient;
 
-    public AuthController(JwtUtil jwtUtil, BCryptPasswordEncoder passwordEncoder, UserServiceClient userServiceClient1) {
+    public AuthController(JwtUtil jwtUtil, UserServiceClient userServiceClient1) {
         this.jwtUtil = jwtUtil;
-        this.passwordEncoder = passwordEncoder;
         this.userServiceClient = userServiceClient1;
     }
 
     // Register endpoint
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody UserDTO userDTO) {
-        return userServiceClient.registerUser(userDTO);
+    public ResponseEntity<Optional<ResponseUserDTO>> register(@RequestBody RegisterUserDTO registerUserDTO) {
+        try {
+            return userServiceClient.registerUser(registerUserDTO);
+        } catch (FeignException.FeignClientException.BadRequest e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (FeignException.FeignClientException.InternalServerError e) {
+            // e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     // Login endpoint
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-        ResponseEntity<String> response = userServiceClient.loginUser(loginRequest);
-
-        if(response.getStatusCode() == HttpStatus.OK){
+    public ResponseEntity<TokenDTO> login(@RequestBody LoginUserDTO loginUserDTO) {
+        try {
+            ResponseEntity<Optional<ResponseUserDTO>> response = userServiceClient.loginUser(loginUserDTO);
+            assert Objects.requireNonNull(response.getBody()).isPresent();
             // Generate JWT token
-            String token = jwtUtil.generateToken(loginRequest.getUsername());
-            return ResponseEntity.ok(token);
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
+            String token = jwtUtil.generateToken(loginUserDTO.getUsername());
+            TokenDTO tokenResponse = TokenDTO.builder()
+                    .token(token)
+                    .username(response.getBody().get().getUsername())
+                    .email(response.getBody().get().getEmail())
+                    .build();
+            return ResponseEntity.ok(tokenResponse);
+        } catch (FeignException.FeignClientException.BadRequest e) {
+            TokenDTO tokenResponse = TokenDTO.builder()
+                    .error("bad request")
+                    .build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(tokenResponse);
+        } catch (FeignException.FeignClientException.Unauthorized e) {
+            TokenDTO tokenResponse = TokenDTO.builder()
+                    .error("username, email or password is wrong")
+                    .build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(tokenResponse);
         }
-
     }
-
 }
-
---
+++ auth-api/src/main/java/com/fridgify/auth_api/dto/ResponseUserDTO.java
@@ -0,0 +1,15 @@
+package com.fridgify.auth_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseUserDTO {
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
