b49a23bb84d7dd97b5ec72f0cedca29f800fa8b7
Enceladus-py <dalsunaberat@gmail.com>
2024-11-13 13:19:11 +0300

    fix: feign client naming

--- auth-api/src/main/java/com/fridgify/auth_api/client/UserServiceClient.java
+++ auth-api/src/main/java/com/fridgify/auth_api/client/UserServiceClient.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 
-@FeignClient(name = "user-api", url = "http://localhost:8082")
+@FeignClient(name = "user-api")
 public interface UserServiceClient {
 
     @PostMapping("/user-api/register")
