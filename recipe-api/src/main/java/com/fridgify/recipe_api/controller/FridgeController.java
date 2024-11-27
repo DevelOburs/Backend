5919c2b863b5f968323bc7a425c8e38e1748fe97
bilgecelikk <jbfmfb@gmail.com>
2024-11-27 23:34:20 +0300

    fridge functions modified for more control over db

--- recipe-api/src/main/java/com/fridgify/recipe_api/controller/FridgeController.java
+++ recipe-api/src/main/java/com/fridgify/recipe_api/controller/FridgeController.java
@@ -1,9 +1,9 @@
 package com.fridgify.recipe_api.controller;
 
-
-import com.fridgify.recipe_api.dto.FridgeDTO;
 import com.fridgify.recipe_api.dto.IngredientDTO;
+import com.fridgify.recipe_api.dto.IngredientIdsDTO;
 import com.fridgify.recipe_api.service.FridgeService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,26 +21,55 @@ public class FridgeController {
     }
 
     @GetMapping("{userId}/inFridge")
-    public ResponseEntity<FridgeDTO> getAllIngredientsInFridge(@PathVariable Long userId) {
-        return new ResponseEntity<>(fridgeService.getAllIngredientsInFridge(userId), HttpStatus.OK);
+    public ResponseEntity<List<IngredientDTO>> getAllIngredientsInFridge(@PathVariable Long userId,
+                                                               @RequestParam(required = false) String nameFilter,
+                                                               @RequestParam(required = false) List<String> categoryFilters) {
+        return new ResponseEntity<>(fridgeService.getAllIngredientsInFridge(userId, nameFilter, categoryFilters), HttpStatus.OK);
+    }
+
+    @GetMapping("{userId}/inFridgePaginated")
+    public ResponseEntity<Page<IngredientDTO>> getAllIngredientsInFridgePaginated(
+            @PathVariable Long userId,
+            @RequestParam(required = false) String nameFilter,
+            @RequestParam(required = false) List<String> categoryFilters,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return new ResponseEntity<>(fridgeService.getAllIngredientsInFridge(userId, nameFilter, categoryFilters, page, size), HttpStatus.OK);
+    }
+
+    @GetMapping("{userId}/inFridge/{ingredientCategory}")
+    public ResponseEntity<List<IngredientDTO>> getAllIngredientsInFridgeByCategory(@PathVariable Long userId,
+                                                                      @PathVariable String ingredientCategory) {
+        return new ResponseEntity<>(fridgeService.getAllIngredientsInFridge(userId, null, List.of(ingredientCategory)), HttpStatus.OK);
     }
 
     @GetMapping("{userId}/notInFridge")
-    public ResponseEntity<List<IngredientDTO>> getIngredientsNotInFridge(@PathVariable Long userId) {
-        return new ResponseEntity<>(fridgeService.getIngredientsNotInFridge(userId), HttpStatus.OK);
+    public ResponseEntity<List<IngredientDTO>> getIngredientsNotInFridge(@PathVariable Long userId,
+                                                                         @RequestParam(required = false) String nameFilter,
+                                                                         @RequestParam(required = false) List<String> categoryFilters) {
+        return new ResponseEntity<>(fridgeService.getIngredientsNotInFridge(userId, nameFilter, categoryFilters), HttpStatus.OK);
     }
 
-    @PostMapping("{userId}/add")
-    public ResponseEntity<FridgeDTO> addIngredientsToFridge(
+    @GetMapping("{userId}/notInFridgePaginated")
+    public ResponseEntity<Page<IngredientDTO>> getIngredientsNotInFridge(@PathVariable Long userId,
+                                                                         @RequestParam(required = false) String nameFilter,
+                                                                         @RequestParam(required = false) List<String> categoryFilters,
+                                                                         @RequestParam(defaultValue = "0") int page,
+                                                                         @RequestParam(defaultValue = "10") int size) {
+        return new ResponseEntity<>(fridgeService.getIngredientsNotInFridge(userId, nameFilter, categoryFilters, page, size), HttpStatus.OK);
+    }
+
+    @PutMapping("{userId}/add")
+    public ResponseEntity<List<IngredientDTO>> addIngredientsToFridge(
             @PathVariable Long userId,
-            @RequestBody List<IngredientDTO> ingredientDTOs) {
-        return new ResponseEntity<>(fridgeService.addIngredientsToFridge(userId, ingredientDTOs), HttpStatus.OK);
+            @RequestBody IngredientIdsDTO request) {
+        return new ResponseEntity<>(fridgeService.addIngredientsToFridge(userId, request.getIngredientIds()), HttpStatus.OK);
     }
 
     @DeleteMapping("{userId}/remove")
-    public ResponseEntity<FridgeDTO> removeIngredientsFromFridge(
+    public ResponseEntity<List<IngredientDTO>> removeIngredientsFromFridge(
             @PathVariable Long userId,
-            @RequestBody List<IngredientDTO> ingredientIds) {
-        return new ResponseEntity<>(fridgeService.removeIngredientsFromFridge(userId, ingredientIds), HttpStatus.OK);
+            @RequestBody IngredientIdsDTO request) {
+        return new ResponseEntity<>(fridgeService.removeIngredientsFromFridge(userId, request.getIngredientIds()), HttpStatus.OK);
     }
 }
